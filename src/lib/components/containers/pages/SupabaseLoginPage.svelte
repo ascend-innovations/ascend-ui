0190401d02185b5a3522a9cfd9aa454c952c1098
zthurmond-ascend <zthurmond@ascend-innovations.com>
2025-02-25 13:49:53 -0500

    Added Supabase login pages

---
+++ src/lib/components/containers/pages/SupabaseLoginPage.svelte
@@ -0,0 +1,56 @@
+<script>
+	import { Button, FormTextInput, LoginPage, InputError } from '$lib/index.js'
+
+	export let form
+</script>
+
+<LoginPage>
+	<form
+		method="post"
+		name="login"
+		action="?/login"
+	>
+		<FormTextInput
+			label="email"
+			type="email"
+			name="email"
+		/>
+		<FormTextInput
+			label="password"
+			type="password"
+			name="password"
+		/>
+
+		{#if form?.login_message}
+			<InputError text={form.login_message} />
+		{/if}
+
+		<div class="login-button-row">
+			<input
+				class={`btn-full btn-l btn-primary btn-rect	semibold`}
+				type="submit"
+				value="Login"
+			/>
+		</div>
+	</form>
+</LoginPage>
+
+<style>
+	.login-button-row {
+		width: 100%;
+	}
+	form {
+		align-items: center;
+		display: flex;
+		flex-direction: column;
+		gap: var(--spacing10);
+		width: 100%;
+	}
+	input[type='submit'] {
+		align-items: center;
+		border-radius: var(--btn-border-radius);
+		border: none;
+		cursor: pointer;
+		text-decoration: none;
+	}
+</style>
---
+++ src/lib/components/containers/pages/SupabaseOTPLoginPage.svelte
@@ -0,0 +1,70 @@
+<script>
+	import { FormTextInput, LoginPage, InputError } from '$lib/index.js'
+
+	export let form
+
+	let otp = ''
+</script>
+
+<LoginPage>
+	<form
+		method="POST"
+		action="?/verifyOtp"
+	>
+		<FormTextInput
+			name="otp"
+			bind:value={otp}
+			label="One-Time Passcode"
+			type="password"
+			required
+		/>
+
+		{#if form?.otp_auth_message}
+			<InputError text={form.otp_auth_message} />
+		{/if}
+
+		<div class="login-button-row">
+			<input
+				class="btn-full btn-l btn-primary btn-rect semibold"
+				type="submit"
+				value="Verify"
+			/>
+		</div>
+	</form>
+	<form
+		method="POST"
+		action="?/resendOtp"
+	>
+		{#if form?.otp_send_message}
+			<InputError text={form.otp_send_message} />
+		{/if}
+
+		<div class="login-button-row resend-section">
+			<input
+				class="btn-link btn-full resend-button semibold"
+				type="submit"
+				value="Resend Code"
+			/>
+		</div>
+	</form>
+</LoginPage>
+
+<style>
+	.login-button-row {
+		width: 100%;
+	}
+	form {
+		align-items: center;
+		display: flex;
+		flex-direction: column;
+		gap: var(--spacing10);
+		width: 100%;
+	}
+	input[type='submit'] {
+		align-items: center;
+		border-radius: var(--btn-border-radius);
+		border: none;
+		cursor: pointer;
+		text-decoration: none;
+	}
+</style>
