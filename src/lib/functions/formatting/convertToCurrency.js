4af674fea665b404fc96f4907ae841d638fc162e
Jesse Hill <jhill@ascend-innovations.com>
2025-03-12 16:23:12 -0400

    removed console logs

--- src/lib/functions/formatting/convertToCurrency.js
+++ src/lib/functions/formatting/convertToCurrency.js
@@ -1,6 +1,3 @@
 export default function convertToCurrency(number) {
-    console.log(number)
-    const currency = number.toLocaleString('en-US', { style: 'currency', currency: 'USD' })
-    console.log(currency)
-    return currency
+    return number.toLocaleString('en-US', { style: 'currency', currency: 'USD' })
 }
