235323612c92452bafc2a42550eddc711e117845
zthurmond-ascend <zthurmond@ascend-innovations.com>
2025-03-27 15:42:41 -0400

    Updated ascend-ui Login for supabase

--- src/lib/functions/auth/login.js
+++ src/lib/functions/auth/login.js
@@ -8,7 +8,27 @@ export default async function login(supabase, email, password) {
         return { success: false, message: 'Invalid email and/or password' }
     }
 
-    // Step 2: Mark the user as starting MFA
+    // Step 2: Check if profile is active
+    let profileData = await supabase
+        .from('profiles')
+        .select('active')
+        .eq('id', loginResponse.data.user.id)
+        .single()
+    let profileError = profileData.error
+
+    if (profileError) {
+        console.error('Profile check error:', profileError.message)
+        return { success: false, message: 'Failed to check user profile.' }
+    }
+
+    if (profileData.data && profileData.data.active === false) {
+        console.log('User profile is inactive.')
+        // Sign out the user
+        await supabase.auth.signOut()
+        return { success: false, message: 'Your account is inactive. Please contact your administrator for assistance.' }
+    }
+
+    // Step 3: Mark the user as starting MFA
     let userUpdateResponse = await supabase.auth.updateUser({
         data: { mfa_completed: false },
     })
@@ -19,7 +39,7 @@ export default async function login(supabase, email, password) {
         return { success: false, message: 'Failed to update authentication status.' }
     }
 
-    // Step 3: Send OTP
+    // Step 4: Send OTP
     let otpResponse = await supabase.auth.signInWithOtp({
         email,
         options: { shouldCreateUser: false },
@@ -31,6 +51,6 @@ export default async function login(supabase, email, password) {
         return { success: false, message: 'Failed to send OTP. Try again.' }
     }
 
-    // Step 4: Redirect to OTP verification page
+    // Step 5: Redirect to OTP verification page
     return { success: true }
 }
