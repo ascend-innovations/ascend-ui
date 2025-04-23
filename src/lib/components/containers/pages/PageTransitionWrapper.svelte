28b9c4d179a488c14d21985d857b94d6330b262d
Jesse Hill <jhill@ascend-innovations.com>
2025-04-23 16:44:17 -0400

    added styles prop to transition wrapper

--- src/lib/components/containers/pages/PageTransitionWrapper.svelte
+++ src/lib/components/containers/pages/PageTransitionWrapper.svelte
@@ -1,11 +1,20 @@
 <script>
     import { fade, fly } from 'svelte/transition'
+    export let styles = []
 </script>
 
 <div
+    class="animation"
     in:fly={{ y: 50, duration: 1000, delay: 500 }}
     out:fade={{ duration: 500 }}
     on:outroend={() => window.scrollTo(0, 0)}
+    style={styles.join(';')}
 >
     <slot />
 </div>
+
+<style>
+    .animation {
+        width: 100%;
+    }
+</style>
