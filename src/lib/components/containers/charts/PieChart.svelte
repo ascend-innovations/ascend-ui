8fd39e738b02968cb39a4bf97e51c612adf20cfd
Jesse Hill <jhill@ascend-innovations.com>
2025-04-24 13:43:02 -0400

    updated pie chart tooltip positioning

--- src/lib/components/containers/charts/PieChart.svelte
+++ src/lib/components/containers/charts/PieChart.svelte
@@ -86,9 +86,11 @@
 	}
 
 	function movingTooltip(e, d, i) {
-		const [x, y] = d3.pointer(e)
-		tooltipData.y = e.offsetY - 85
-		tooltipData.x = e.offsetX - 10
+		let tooltipHeight = tooltip.node().getBoundingClientRect().height
+		let tooltipWidth = tooltip.node().getBoundingClientRect().width
+		
+		tooltipData.y = e.offsetY - tooltipHeight - 20
+		tooltipData.x = e.offsetX - tooltipWidth / 2
 		tooltipData.title = d.data[domain]
 		tooltipData.valueOne = d.data[range]
 		if (valueTwoLabel) tooltipData.valueTwo = d.data[valueTwoLabel]
