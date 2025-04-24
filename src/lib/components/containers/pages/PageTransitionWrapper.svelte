54cab71ead54eb092fa0f52ff55b43fb27ddaa0b
Jesse Hill <jhill@ascend-innovations.com>
2025-04-24 10:31:13 -0400

    made fly in delay optional

--- src/lib/components/containers/pages/PageTransitionWrapper.svelte
+++ src/lib/components/containers/pages/PageTransitionWrapper.svelte
@@ -1,11 +1,11 @@
 <script>
     import { fade, fly } from 'svelte/transition'
-    export let styles = []
+    export let styles = [], delayIn = true
 </script>
 
 <div
     class="animation"
-    in:fly={{ y: 50, duration: 1000, delay: 500 }}
+    in:fly={{ y: 50, duration: 1000, delay: delayIn ? 500 : 0 }}
     out:fade={{ duration: 500 }}
     on:outroend={() => window.scrollTo(0, 0)}
     style={styles.join(';')}
