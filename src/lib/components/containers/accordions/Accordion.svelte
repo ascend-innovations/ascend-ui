69a06d1e478ec8ce4b2a04ed325bdbbb010a5c91
Jesse Hill <jhill@ascend-innovations.com>
2025-04-25 15:38:34 -0400

    fixed defaultOpen in Accordion

--- src/lib/components/containers/accordions/Accordion.svelte
+++ src/lib/components/containers/accordions/Accordion.svelte
@@ -7,32 +7,32 @@
 		defaultOpen = false
 
 	let detailsElement,
+		summaryElement,
 		open = defaultOpen
 
 	function detailsClick(e) {
-		detailsElement = document.getElementById(e.target.id)
-		open = !detailsElement.parentElement.open
+		open = !summaryElement.parentElement.open
 	}
 
 	onMount(() => {
 		if (browser) {
-			const detailsElement = document.querySelector('.category-details')
 			detailsElement.open = defaultOpen
 		}
 	})
 </script>
 
-<details class="category-details">
+<details bind:this={detailsElement} class="category-details">
 	<!-- svelte-ignore a11y-click-events-have-key-events -->
 	<!-- svelte-ignore a11y-no-static-element-interactions -->
 	<summary
+		bind:this={summaryElement}
 		id={`${title}-details`}
 		class="option-title"
 		on:click={(e) => detailsClick(e)}
 	>
 		<h3 class="headline-l-s">{title}</h3>
 		<span class="summary-icon">
-			{#if detailsElement?.id === `${title}-details` && open}
+			{#if summaryElement?.id === `${title}-details` && open}
 				<svelte:component this={ChevronSingleUpSmallIcon} />
 			{:else}
 				<svelte:component this={ChevronSingleDownSmallIcon} />
