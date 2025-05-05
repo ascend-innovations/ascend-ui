845f84239a434614593af3a361e7a5bd194e87e2
CollinLFerguson <collinleeferguson@gmail.com>
2025-05-05 15:06:42 -0400

    adds testing to sanitizeUserInput, validateEmail, and validatePassword. Also fixes one edge rate case on sanitizeUserInput

---
+++ tests/unit/functions/security/validateCredentials.test.js
@@ -0,0 +1,28 @@
+import { describe, it, expect } from "vitest";
+import { validateCredentials } from "$lib";
+
+const functionName = "validateCredentials"
+
+describe(`Tests the operations of ${functionName} with proper inputs`, ()=>{
+	it(`Tests ${functionName} using proper input`, ()=> {
+		true
+	})
+})
+
+describe(`Tests the operations of ${functionName} with inproper inputs`, ()=>{
+	it(`Tests ${functionName} using inproper input`, ()=> {
+		true
+	})
+})
+
+describe(`Tests ${functionName} for additional special cases:`, ()=>{
+	it(`Tests ${functionName} for additional special case: [case]:`, ()=>{ //repeat as need.
+		true
+	})
+})
+
+/*
+export default function validateCredentials(email, password) {
+	return validateEmail(email) && validatePassword(password)
+}
+*/
