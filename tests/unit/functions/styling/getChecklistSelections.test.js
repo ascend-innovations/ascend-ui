a2bd2001cbe606acca50d91f397b266d3652bb0a
CollinLFerguson <collinleeferguson@gmail.com>
2025-05-06 15:09:10 -0400

    adds test for checlistSelections. adds fix for truthy non-booleans being counted as selected checkboxes.

---
+++ tests/unit/functions/styling/getChecklistSelections.test.js
@@ -0,0 +1,45 @@
+import { describe, it, expect } from "vitest";
+import { getChecklistSelections } from "$lib";
+
+const functionName = " "
+
+describe(`Tests the operations of ${functionName} with proper inputs`, ()=>{
+	it(`Tests ${functionName} using proper input`, ()=> {
+		let list = [
+			{ valuee: "O1", value: 'Option1', selected: true},
+			{ valuee: "O2", value: 'Option2', selected: true},
+			{ valuee: "O3", value: 'Option3', selected: true},
+			{ valuee: "O4", value: 'Option4', selected: false},
+			{ valuee: "O5", value: 'Option5', selected: false},
+			{ valuee: "O6", value: 'Option6', selected: false},
+			{ valuee: "O7", value: 'Option7', selected: false}]
+			expect(getChecklistSelections(list)).toEqual(['Option1', 'Option2', 'Option3'])
+			expect(getChecklistSelections(list, 'valuee')).toEqual(['O1', 'O2', 'O3'])
+	})
+})
+
+describe(`Tests the operations of ${functionName} with inproper inputs`, ()=>{
+	it(`Tests ${functionName} using inproper input`, ()=> {
+		let list = [
+			{ valuee: "O1", value: 'Option1', selected: true},
+			{ valuee: "O2", value: 'Option2', selected: true},
+			{ valuee: "O3", value: 'Option3', selected: true},
+			{ valuee: "O4", value: 'Option4', selected: undefined},
+			{ valuee: "O5", value: 'Option5', selected: null},
+			{ valuee: "O6", value: 'Option6', selected: ""},
+			{ valuee: "O7", value: 'Option7', selected: {}},
+			{ valuee: "O7", value: 'Option7', selected: 'hello'},
+			{ valuee: "O7", value: 'Option7', selected: 'false'},
+			{ valuee: "O7", value: 'Option7', selected: -1},
+			{ valuee: "O7", value: 'Option7', selected: 0},
+			]
+			expect(getChecklistSelections(list)).toEqual(['Option1', 'Option2', 'Option3'])
+			expect(getChecklistSelections(list, 'valuee')).toEqual(['O1', 'O2', 'O3'])
+	})
+})
+
+describe(`Tests ${functionName} for additional special cases:`, ()=>{
+	it(`Tests ${functionName} for additional special case: [case]:`, ()=>{ //repeat as need.
+		true
+	})
+})
