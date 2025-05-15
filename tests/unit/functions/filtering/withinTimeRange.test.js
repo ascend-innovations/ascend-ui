a2ae8dd7d26427ac2e1536a44a04e86d5e7746cb
Collin Ferguson <140536140+CollinLFerguson@users.noreply.github.com>
2025-05-15 09:03:18 -0400

    Update withinTimeRange.test.js
    Signed-off-by: Collin Ferguson <140536140+CollinLFerguson@users.noreply.github.com>

--- tests/unit/functions/filtering/withinTimeRange.test.js
+++ tests/unit/functions/filtering/withinTimeRange.test.js
@@ -6,9 +6,9 @@ const functionName = "withinTimeRange"
 describe(`Tests the operations of ${functionName} with proper inputs`, ()=>{
 	it(`Tests ${functionName} using proper input`, ()=> {
 		expect(withinTimeRange("10:00", "12:00", "11:00" )).toBe(true)
-        expect(withinTimeRange("12:00", "10:00", "13:00" )).toBe(true)
+        	expect(withinTimeRange("12:00", "10:00", "13:00" )).toBe(true)
 
-        expect(withinTimeRange("10:00", "12:00", "13:00" )).toBe(false)
+        	expect(withinTimeRange("10:00", "12:00", "13:00" )).toBe(false)
 		expect(withinTimeRange("12:00", "10:00", "11:00" )).toBe(false)
 	})
 })
@@ -16,27 +16,27 @@ describe(`Tests the operations of ${functionName} with proper inputs`, ()=>{
 describe(`Tests the operations of ${functionName} with inproper inputs`, ()=>{
 	it(`Tests ${functionName} using inproper input`, ()=> {
 		expect(withinTimeRange("", "12:00", "11:00" )).toBe(true)
-        expect(withinTimeRange("10:00", "", "11:00" )).toBe(true)
-        expect(withinTimeRange("12:00", "10:00", "" )).toBe(false)
+        	expect(withinTimeRange("10:00", "", "11:00" )).toBe(true)
+        	expect(withinTimeRange("12:00", "10:00", "" )).toBe(false)
 
-        expect(withinTimeRange("", "12:00", "13:00" )).toBe(false)
+        	expect(withinTimeRange("", "12:00", "13:00" )).toBe(false)
 		expect(withinTimeRange("12:00", "", "11:00" )).toBe(false)
 
 		expect(withinTimeRange(["10:00"], "12:00", "11:00" )).toBe(true)
-        expect(withinTimeRange("12:00", ["10:00"], "13:00" )).toBe(true)
-        expect(withinTimeRange("12:00", "10:00", ["13:00"] )).toBe(true)
+        	expect(withinTimeRange("12:00", ["10:00"], "13:00" )).toBe(true)
+        	expect(withinTimeRange("12:00", "10:00", ["13:00"] )).toBe(true)
 
-        expect(withinTimeRange(["10:00"], "12:00", "13:00" )).toBe(false)
+        	expect(withinTimeRange(["10:00"], "12:00", "13:00" )).toBe(false)
 		expect(withinTimeRange("12:00", ["10:00"], "11:00" )).toBe(false)
 		expect(withinTimeRange("10:00", "12:00", ["13:00"] )).toBe(false)
 
-        expect(withinTimeRange({}, {}, "13:00" )).toBe(false)
+        	expect(withinTimeRange({}, {}, "13:00" )).toBe(false)
 		expect(withinTimeRange("12:00", ["10:00"], "11:00" )).toBe(false)
 		expect(withinTimeRange("10:00", "12:00", ["13:00"] )).toBe(false)
 
 		expect(withinTimeRange(null, "12:00", "11:00" )).toBe(true)
-        expect(withinTimeRange("10:00", null, "11:00" )).toBe(true)
-        expect(withinTimeRange("12:00", "10:00", null )).toBe(false)
+        	expect(withinTimeRange("10:00", null, "11:00" )).toBe(true)
+        	expect(withinTimeRange("12:00", "10:00", null )).toBe(false)
 	})
 })
 
