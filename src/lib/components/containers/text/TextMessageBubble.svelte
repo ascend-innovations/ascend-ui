dd03c5dd236272692b0bd5434d95b400ce771de3
Collin Ferguson <collinleeferguson@gmail.com>
2025-06-13 09:25:36 -0400

    refactored textMessageBubbleElement

--- src/lib/components/containers/text/TextMessageBubble.svelte
+++ src/lib/components/containers/text/TextMessageBubble.svelte
@@ -1,20 +1,29 @@
 <script>
-	export let height = '',
+	import { onMount } from 'svelte';
+
+	export let id='', 
+		height = '',
 		width = '',
 		padding = ''
+
+
+	onMount(() => {
+		let el = document.getElementById(`${id}-text-bubble`);
+		if (!el) return; 
+		if(height){el.style.height = height;}
+		if(width){el.style.width = width;}
+		
+		let bubbleContent = el.querySelector(".text-bubble__content");
+		if (bubbleContent && padding) {
+			bubbleContent.style.padding = padding;
+		}
+	});
+
+
 </script>
 
-<div
-	class="text-bubble"
-	style={`
-		${`height:${height}`}
-		${`width:${width}`}
-	`}
->
-	<div
-		class="text-bubble__content"
-		style={`padding:${padding}`}
-	>
+<div id={`${id}-text-bubble`} class="text-bubble">
+	<div class="text-bubble__content">
 		<slot />
 	</div>
 	<div class="text-bubble__arrow" />
