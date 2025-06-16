29dd4af7f58f28b11fc6708c94808257d25aea13
Jesse Hill <jhill@ascend-innovations.com>
2025-06-16 10:41:18 -0400

    refactored supabaseotploginpage to remove ascend-ui dependencies.

--- src/lib/components/containers/pages/SupabaseOtpLoginPage.svelte
+++ src/lib/components/containers/pages/SupabaseOtpLoginPage.svelte
@@ -1,11 +1,12 @@
 <script>
-	import { FormTextInput, LoginPage, InputError, validateSupabaseOtp } from '$lib/index.js'
+	import { browser } from '$app/environment'
+	import { InputError, validateSupabaseOtp, AscendLoginBackground, AscendLogoWithText } from '$lib/index.js'
 	import { createEventDispatcher } from 'svelte'
 	import { onMount } from 'svelte'
 
 	export let form
 
-	let otp, validOtp, otpValidationMessage
+	let otp = '', validOtp, errorSlot, resendErrorSlot
 
 	const dispatch = createEventDispatcher()
 
@@ -13,10 +14,16 @@
 		validOtp = validateSupabaseOtp(otp)
 
 		if (!otp) {
-			otpValidationMessage = 'Please enter your One-Time Passcode'
+			new InputError({
+				target: errorSlot,
+				props: { text: 'Please enter your One-Time Passcode' }
+			})
 		} else {
 			if (!validOtp) {
-				otpValidationMessage = 'Invalid passcode. Please try again'
+				new InputError({
+					target: errorSlot,
+					props: { text: 'Invalid passcode. Please try again' }
+				})
 			}
 		}
 
@@ -24,81 +31,109 @@
 	}
 
 	onMount(() => {
+		if (browser) {
+			errorSlot = document.querySelector('.sso-login__error')
+			resendErrorSlot = document.querySelector('.sso-login__resend-error')
+
+			if (form?.message) {
+				errorSlot.style.display = 'block'
+
+				new InputError({
+					target: errorSlot,
+					props: { text: form?.message }
+				})
+			} else {
+				errorSlot.style.display = 'none'
+			}
+		}
+
 		if (form?.otp_resend_message) {
 			dispatch('otpResent', form)
+
+			resendErrorSlot.style.display = 'block'
+			new InputError({
+				target: resendErrorSlot,
+				props: { text: form?.otp_resend_message }
+			})
 		}
 	})
 </script>
 
-<LoginPage
-	loginHeader="Enter Passcode"
-	ascendLogo
+
+<div
+	class="login-page"
+	style={`background-image: url(${AscendLoginBackground})`}
 >
-	<div class="sso-otp__form-container">
-		<form
-			class="sso-otp__form"
-			method="POST"
-			action="?/verifyOtp"
-		>
-			<p class="neutral-400-text text-align-center">Enter the one-time passcode sent to your email to continue.</p>
-			<FormTextInput
-				name="otp"
-				bind:value={otp}
-				label="One-Time Passcode"
-				validValue={validOtp}
-				validationCallback={otpValidation}
-				validationMessage={otpValidationMessage}
+	<div class="login-card">
+		<div class="login-card__ascend-icon width-100">
+			<img
+				src={AscendLogoWithText}
+				alt="ascend-logo"
 			/>
-			<input
-				value={password}
-				class="form-text__input {validPassword === false ? 'form-text__input--error' : ''}"
-				id="password"
-				name="password"
-				placeholder="Enter your password"
-				type="password"
-			/>
+		</div>
+		<div class="login-card__header width-100 text-align-center">Login</div>
+		<div class="sso-otp__form-container">
+			<form
+				class="sso-otp__form"
+				method="POST"
+				action="?/verifyOtp"
+			>
+				<p class="neutral-400-text text-align-center">Enter the one-time passcode sent to your email to continue.</p>
+				<div class="form-text">
+					<label
+						for="otp"
+						class="body-m-semibold"
+					>
+						One-Time Passcode
+					</label>
+					<input
+						value={otp}
+						class="form-text__input {validOtp === false ? 'form-text__input--error' : ''}"
+						id="otp"
+						name="otp"
+						placeholder="Enter your one-time passcode"
+						type="password"
+					/>
+				</div>
 
-			{#if form?.message}
-				<InputError text={form.message} />
-			{/if}
+				<div class="sso-login__error" style="display:none;"></div>
 
-			<div class="sso-otp__button">
-				<input
-					class="btn-full btn-l btn-primary btn-rect semibold sso-otp__submit"
-					type="submit"
-					value="Verify"
-				/>
-			</div>
-		</form>
-		<form
-			class="sso-otp__resend-form"
-			method="POST"
-			action="?/resendOtp"
-		>
-			{#if form?.resend_message}
-				<InputError text={form.resend_message} />
-			{/if}
+				<div class="sso-otp__button">
+					<input
+						class="btn-full btn-l btn-primary btn-rect semibold sso-otp__submit"
+						type="submit"
+						value="Verify"
+					/>
+				</div>
+			</form>
+			<form
+				class="sso-otp__resend-form"
+				method="POST"
+				action="?/resendOtp"
+			>
+				<div class="sso-login__resend-error"></div>
 
-			<div class="sso-otp__button sso-otp__resend-section">
-				<p class="body-xs">Didn't get an email?</p>
-				<input
-					class="btn-link semibold body-xs neutral-400-text"
-					type="submit"
-					value="Resend Code"
-				/>
-			</div>
-			<div class="sso-otp__miscellaneous-links">
-				<a
-					class="body-xs"
-					target="_blank"
-					href="https://ascend-innovations.com/legal/terms-and-conditions"><b>Terms & Conditions</b></a
-				>
-				<a
-					class="body-xs"
-					target="_blank"
-					href="https://ascend-innovations.com/legal/privacy-policy"><b>Privacy Policy</b></a
-				>
-			</div>
-		</form>
+				<div class="sso-otp__button sso-otp__resend-section">
+					<p class="body-xs">Didn't get an email?</p>
+					<input
+						class="btn-link semibold body-xs neutral-400-text"
+						type="submit"
+						value="Resend Code"
+					/>
+				</div>
+				<div class="sso-otp__miscellaneous-links">
+					<a
+						class="body-xs"
+						target="_blank"
+						href="https://ascend-innovations.com/legal/terms-and-conditions"><b>Terms & Conditions</b></a
+					>
+					<a
+						class="body-xs"
+						target="_blank"
+						href="https://ascend-innovations.com/legal/privacy-policy"><b>Privacy Policy</b></a
+					>
+				</div>
+			</form>
+		</div>
 	</div>
-</LoginPage>
+</div>
